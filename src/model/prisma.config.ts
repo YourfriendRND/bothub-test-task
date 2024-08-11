189e0e4af3ff2b0aff358726a1a2e5780981e2e1
YourfriendRND <kupeirc@gmail.com>
2024-08-11 23:27:59 +0400

    wip: подготовка БД и описание моделей

---
+++ src/model/prisma.config.ts
@@ -0,0 +1,68 @@
+import joi from 'joi'; 
+import { config } from 'dotenv';
+import { writeFile, unlink } from 'fs/promises';
+import { PrismaConfigSchema } from './../types'
+
+const validationSchema = joi.object({
+    POSTGRES_USER: joi.string().required(),
+    POSTGRES_PASSWORD: joi.string().required(),
+    POSTGRES_HOST: joi.string().required(),
+    POSTGRES_PORT: joi.number().required(),
+    POSTGRES_DB: joi.string().required(),
+})
+
+function validateConfig(config: PrismaConfigSchema): void {
+    const { error } = validationSchema.validate(config, { abortEarly: true });
+
+    if (error) {
+        throw new Error(`[Application Config Validation Error]: ${error.message}`)
+    }
+}
+
+function getAppConfig(): PrismaConfigSchema {
+    const appConfig: PrismaConfigSchema = {
+        POSTGRES_USER: process.env['POSTGRES_USER']!,
+        POSTGRES_PASSWORD: process.env['POSTGRES_PASSWORD']!,
+        POSTGRES_HOST: process.env['POSTGRES_HOST']!,
+        POSTGRES_PORT: parseInt(process.env['POSTGRES_PORT']!, 10),
+        POSTGRES_DB: process.env['POSTGRES_DB']!
+    };
+
+    validateConfig(appConfig);
+
+    return appConfig;
+}
+
+function createConnectionString(config: PrismaConfigSchema): string {
+    return `postgresql://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}:${config.POSTGRES_PORT}/${config.POSTGRES_DB}?schema=public`
+}
+
+function createEnvConfig(config: Record<string, unknown>): string {
+    return Object.entries(config).map(([key, value]) => `${key}=${value}`).join('\n');
+}
+
+async function updateAppEnvFile(configContent: string): Promise<void> {
+    try {
+        await unlink('./src/model/.env');
+    } catch {
+        console.error('Prisma .env file does not exist');
+    }
+    finally {
+        await writeFile('./src/model/.env', configContent);
+    }
+}
+
+async function generate(): Promise<void> {
+    config()
+    const envConfig = getAppConfig();
+    const dbConnectionString = createConnectionString(envConfig);
+    const configContent = createEnvConfig({
+        ...{
+            DATABASE_URL: dbConnectionString,
+        }
+    })
+    await updateAppEnvFile(configContent);
+    console.log('File .env successfuly updated 👍');
+}
+
+generate();
---
+++ src/types/config/index.ts
@@ -0,0 +1,2 @@
+export * from './application-config.type';
+export * from './prisma-config.type';
